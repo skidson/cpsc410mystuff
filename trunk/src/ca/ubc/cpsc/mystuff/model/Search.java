63d57186f0106ea6853aa38f15d33f0b809c89b8
jeffpayan@gmail.com <jeffpayan@gmail.com@658a64ba-5a30-478e-aa60-9e8b38c5be45>
2010-12-01 03:14:24 +0000

    asdf

--- trunk/src/ca/ubc/cpsc/mystuff/model/Search.java
+++ trunk/src/ca/ubc/cpsc/mystuff/model/Search.java
@@ -1,33 +1,10 @@
 package ca.ubc.cpsc.mystuff.model;
 
 public class Search {
-	private int duration;
-	private int rating;
-	private String title;
-	private String genre;
+	private String search;
 	
-	public Search(int duration, int rating, String title, String genre){
-		this.duration = duration;
-		this.rating = rating;
-		this.title = title;
-		this.genre = genre;
+	public Search(String search){
+		this.search = search;
 	}
-	
-	public int getDuration(){
-		return (this.duration);
-	}
-	
-	public int getRating(){
-		return (this.rating);
-	}
-	
-	public String getTitle(){
-		return (this.title);
-	}
-	
-	public String getGenre(){
-		return (this.genre);
-	}
-
 	
 }
